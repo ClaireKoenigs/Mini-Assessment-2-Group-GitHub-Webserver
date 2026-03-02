822ccf9c3f3da0a86af4485a29b0b641f392a2fd
Miles <miles.christiansen@southeasttech.edu>
2026-03-02 14:37:44 -0600

    Profile update

---
+++ Profile_File/Profile.js
@@ -0,0 +1,31 @@
+const PicBtn = document.getElementById("PicChange")
+const Imgselector = document.getElementById("ImageSelect")
+PicBtn.addEventListener("click", function () {
+    Imgselector.style.display = "grid";
+});
+const Picture = document.getElementById("Picture")
+document.getElementById("Submit").addEventListener("click", function (event) {
+    const file = document.getElementById("InputFile").files[0]
+
+    if (!file) {
+        alert("File not selceted")
+        return;
+    }
+
+    const reader = new FileReader()
+
+    reader.onload = function (e) {
+        Picture.src = e.target.result;
+    };
+
+    reader.onerror = function () {
+        alert("Error reading file");
+    };
+
+    reader.readAsDataURL(file);
+
+    Imgselector.style.display = "none";
+});
+
+document.getElementById("ProSave").addEventListener("click",function(event){
+})
